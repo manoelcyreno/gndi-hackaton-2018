473368bea332ef9c82d9b570e39fc5d8d94c469d
Jordana Morais <jordana.morais@liferay.com>
2018-10-25 18:02:16 -0300

    Create gndi dashboard portlet

---
+++ modules/gndi-dashboard/src/main/java/gndi/dashboard/portlet/GndiDashboardPortlet.java
@@ -0,0 +1,28 @@
+package gndi.dashboard.portlet;
+
+import gndi.dashboard.constants.GndiDashboardPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author jordanamorais
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + GndiDashboardPortletKeys.GndiDashboard,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class GndiDashboardPortlet extends MVCPortlet {
+}
