d20a6d1ab7717d2fb4813a15edfe480ecc2b2ffb
Paulo Cruz <paulo.cruz@liferay.com>
2018-10-26 09:30:33 -0300

    Create company summary endpoint

---
+++ node/server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const WeDeploy = require('wedeploy');
+
+app.get('/company-summary', (req, res) => {
+    getCompanySummary().then(result => {
+        res.send(JSON.stringify(result));
+    });
+});
+   
+app.listen(3000);
+
+function getCompanySummary() {
+    return WeDeploy
+        .data('https://data-gndihackaton.wedeploy.io/')
+        .get('user')
+        .then(usersList => {
+            let dependentCount = usersList.filter(user => user['isDependent']).length;
+            let ownerCount = usersList.filter(user => !user['isDependent']).length;
+
+            return {
+                "dependentCount": dependentCount,
+                "ownerCount": ownerCount
+            }
+        });
+}
